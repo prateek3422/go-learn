e274b66e47ca03db1d8cdb11f86904c6c2a279fe
Prateek <ps0431299@gmail.com>
2025-07-03 20:15:32 +0530

    go learn start

---
+++ 2_variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "abhi"
+	email := "prateek@gmail.com"
+	var phone int = 414548745412
+	fmt.Println(email, phone)
+}
---
+++ 3_Data_Type/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var otp = 12345
+	name := "prateek"
+	isAdmin := true
+	var price float32 = 51.2
+
+	fmt.Println(otp)
+	fmt.Println(name)
+	fmt.Println(price)
+	fmt.Println(isAdmin)
+
+}
